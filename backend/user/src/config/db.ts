b773364e855ca2091f355d2e2b30fa12d1cba8c2
iam-vinnu <binaybehera101@gmail.com>
2025-10-06 20:31:35 +0530

    db created

--- backend/user/src/config/db.ts
+++ backend/user/src/config/db.ts
@@ -1,2 +1,17 @@
 import mongoose from "mongoose";
 
+export const connectDB = async()=>{
+    const uri = process.env.MONGO_URI;
+    if(!uri) throw new Error("MONGO URI is not derived in the environment variable");
+    
+   try {
+       await mongoose.connect(uri,{
+        dbName : 'userdbconversation'
+    });
+    console.log('MongoDB is Connected');
+    
+   } catch (error) {
+     console.error('Failed to connect to mongodb : ',error);
+     process.exit(1);
+   }
+};
--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import dotenv from 'dotenv'
+import { connectDB } from "./config/db";
 
 dotenv.config({});
 const app = express();
 const port = process.env.PORT || 3000 ;
+
+connectDB();
 app.listen(port,()=>{
     console.log(`Server in litsening at ${port}`);
     
