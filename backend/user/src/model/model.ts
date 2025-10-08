7ee996d1ee124ae3e369b279f1b5bbf87a45819a
iam-vinnu <binaybehera101@gmail.com>
2025-10-08 20:05:39 +0530

    defined user schema

--- backend/user/src/model/model.ts
+++ backend/user/src/model/model.ts
@@ -1,2 +1,25 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import { UserProps } from "../config/interfaces";
 
+const userSchema : Schema<UserProps> = new Schema({
+    email:{
+        type: String,
+        required: true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    userName:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    name:{
+        type:String,
+    },
+    avatar:{
+        type:String
+    }
+},{timestamps: true});
